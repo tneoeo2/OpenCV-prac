1e03424d7ddfd425bab125db0f8a470bdc521396
tneoeo2 <tneoeo2@gmail.com>
2022-04-03 23:42:01 +0900

    img, 001 update

--- opencv_001.py
+++ opencv_001.py
@@ -1,6 +1,13 @@
-    ###? 카메라 출력
+    ###? 카메라 출력/파일 불러오기
 
 import cv2
+import numpy as np
 
 print(cv2.__version__)  #파이썬 버전출력
 
+PATH = "./image/n001.jpg"
+
+img = cv2.imread(PATH)
+cv2.imshow("test", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
